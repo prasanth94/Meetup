09f7958253c5f64c8fc0e15618b161469e61c10a
Prasanth <prasanth@cloudthing.com>
2021-06-19 23:33:42 +0530

    add event details page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,3 +1,30 @@
+import { useRouter } from 'next/router'
+import EventSummary from '../../components/event-detail/event-summary'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventContent from '../../components/event-detail/event-content'
+import { getEventById } from '../../dummy-data'
+
 export default function EventDetailsPage() {
-  return <h3>Event details page</h3>
+  const router = useRouter()
+
+  const event = getEventById(router.query.eventId)
+
+  if (!event) {
+    return <p>No event found</p>
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  )
 }
